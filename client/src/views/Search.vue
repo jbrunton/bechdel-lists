10b8130eccaa9a3fda9c2c911279988238f0be72
John Brunton <jbrunton@zipcar.com>
2020-04-23 18:13:07 +0100

    make request to /api/search

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="search">
     <h1>Search</h1>
+
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">
+        {{ movie.title }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+const axios = require('axios');
+
+export default {
+  data() {
+    return {
+      movies: []
+    }
+  },
+
+  created() {
+    this.load();
+  },
+
+  methods: {
+    async load() {
+      const result = await axios.get('/api/search?query=matrix');
+      this.movies = result.data;
+    }
+  }
+}
+</script>
