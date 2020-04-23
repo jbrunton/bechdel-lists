2f0850bebe34c36ff80a6a1e987063ee1fc59be6
John Brunton <jbrunton@zipcar.com>
2020-04-23 14:03:13 +0100

    add search endpoint

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-const express = require('express')
+const express = require('express');
+const axios = require('axios');
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/search', async (req, res) => {
+    const query = req.query['query'];
+    const results = await axios.get(`http://bechdeltest.com/api/v1/getMoviesByTitle?title=${query}`)
+    res.json(results.data)
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
