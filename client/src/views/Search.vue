ccd432b29c794140bc1859442a3162e7fe443e27
John Brunton <jbrunton@zipcar.com>
2020-04-23 22:36:45 +0100

    add loading indicator

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -5,13 +5,40 @@
     <v-row>
 
       <v-col cols="6">
-        <v-text-field label="Search">
+        <v-text-field
+          label="Search"
+          v-model="query"
+          @change="search"
+          append-icon="mdi-magnify">
         </v-text-field>
       </v-col>
 
       <v-col cols="6">
 
-        <v-list two-line subheader>
+        <v-container style="height: 400px;" v-if="showLoadingIndicator">
+          <v-row
+            class="fill-height"
+            align-content="center"
+            justify="center"
+          >
+            <v-col
+              class="subtitle-1 text-center"
+              cols="12"
+            >
+              Getting your files
+            </v-col>
+            <v-col cols="6">
+              <v-progress-linear
+                color="deep-purple accent-4"
+                indeterminate
+                rounded
+                height="6"
+              ></v-progress-linear>
+            </v-col>
+          </v-row>
+        </v-container>
+
+        <v-list two-line subheader v-else>
           <v-list-item v-for="movie in movies" :key="movie.id" @click="movieSelected">
             <v-list-item-content>
               <v-list-item-title v-text="movie.title"></v-list-item-title>
@@ -26,6 +53,7 @@
             </v-list-item-action>
           </v-list-item>
         </v-list>
+
       </v-col>
 
     </v-row>
@@ -38,21 +66,28 @@ const axios = require('axios');
 export default {
   data() {
     return {
-      movies: []
+      movies: [],
+      showLoadingIndicator: false,
+      query: ""
     }
   },
 
-  created() {
-    this.load();
-  },
-
   methods: {
-    async load() {
-      const result = await axios.get('/api/search?query=matrix');
-      const movies = result.data.map(movie => Object.assign({
-        starRating: this.starRating(movie)
-      }, movie));
-      this.movies = movies;
+    async search() {
+      if (this.query.length >= 3) {
+        this.showLoadingIndicator = true;
+
+        const result = await axios.get(`/api/search?query=${this.query}`);
+        const movies = result.data.map(movie => Object.assign({
+          starRating: this.starRating(movie)
+        }, movie));
+        this.movies = movies;
+
+        this.showLoadingIndicator = false;
+      } else {
+        this.showLoadingIndicator = false;
+        this.movies = [];
+      }
     },
 
     starRating(movie) {
