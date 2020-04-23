bfda76884e0d641c037a07720ef4388d31513e3e
John Brunton <jbrunton@zipcar.com>
2020-04-23 18:48:01 +0100

    add router back

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -15,6 +15,15 @@
           Welcome to Vuetify
         </h1>
 
+        <div id="app">
+          <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link> |
+            <router-link to="/search">Search</router-link>
+          </div>
+          <router-view/>
+        </div>
+
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
           <br>please join our online
