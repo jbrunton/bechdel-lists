7d0254302238e4bd65db9af1df358a56da1da231
John Brunton <jbrunton@zipcar.co.uk>
2020-04-24 13:30:52 +0100

    add database configuration and /lists endpoints

---
+++ api/db.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'postgres',
+  databse: 'bechdel_demo',
+  password: 'postgres_password'
+})
+
+module.exports = {
+  init: () => {
+    pool.on('error', () => console.log('Lost PG connection'));
+
+    pool.query('DROP TABLE IF EXISTS lists');
+    pool.query('CREATE TABLE IF NOT EXISTS lists (id serial PRIMARY KEY, title TEXT)')
+      .catch(err => console.log(err));
+  },
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback)
+  }
+}
--- api/index.js
+++ api/index.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const axios = require('axios');
+const db = require('./db');
 
 const app = express()
 const port = 5000
 
+const isEmpty = (value) => value === null || value === '';
+
+db.init();
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/search', async (req, res) => {
-    const query = req.query['query'];
-    const results = await axios.get(`http://bechdeltest.com/api/v1/getMoviesByTitle?title=${query}`)
-    res.json(results.data)
+  const query = req.query['query'];
+  const results = await axios.get(`http://bechdeltest.com/api/v1/getMoviesByTitle?title=${query}`)
+  res.json(results.data)
+});
+
+app.get('/lists', async (req, res) => {
+  const values = await db.query('SELECT * from lists');
+  res.json(values.rows);
+});
+
+app.post('/lists', async (req, res) => {
+  const title = req.body.title;
+  // if (isNullOrEmpty(title)) {
+
+  // }
+  
+  const result = db.query('INSERT INTO lists(title) VALUES($1) RETURNING *', [title]);
+
+  res.json(result.rows[0])
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
