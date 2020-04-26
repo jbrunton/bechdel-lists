ecfc715eb36af928b044be504dbf2283e9ed21b9
John Brunton <jbrunton@zipcar.co.uk>
2020-04-26 16:17:14 +0100

    DEMO-18 rename views -> pages

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Lists from '../views/Lists.vue'
 
 Vue.use(VueRouter)
 
@@ -8,12 +7,12 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Lists',
-    component: Lists
+    component: () => import(/* webpackChunkName: "lists" */ '../pages/Lists.vue')
   },
   {
     path: '/search',
     name: 'Search',
-    component: () => import(/* webpackChunkName: "search" */ '../views/Search.vue')
+    component: () => import(/* webpackChunkName: "search" */ '../pages/Search.vue')
   }
 ]
 
