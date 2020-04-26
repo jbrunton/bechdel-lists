5e83020b046f3a3ac3e73b1e4791297dffe63e96
John Brunton <jbrunton@zipcar.co.uk>
2020-04-26 14:11:58 +0100

    DEMO-13 load routers automatically

--- api/run.js
+++ api/run.js
@@ -1,4 +1,4 @@
-const app = require('./app');
+const app = require('./config/app');
 
 const port = process.env.SERVER_PORT;
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
