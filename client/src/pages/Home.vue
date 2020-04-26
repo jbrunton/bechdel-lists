80eb38ab5e9b5c4e232eb07ed4fa32d7ccf3c12f
John Brunton <jbrunton@zipcar.co.uk>
2020-04-26 22:24:54 +0100

    DEMO-20 add sign in button to home page

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -18,5 +18,38 @@
       on the average). Bechdel scores are taken from the
       <a href="https://bechdeltest.com/api/v1/doc">bechdeltest.com API</a>.
     </p>
+    <div id="sign-in-button"></div>    
     </v-container>
 </template>
+
+<script>
+/* global gapi */
+
+export default {
+  mounted () {
+    gapi.signin2.render('sign-in-button', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': this.onAuthSuccess,
+        'onfailure': this.onAuthFailure
+      });
+  },
+
+  methods: {
+    onAuthSuccess(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    },
+
+    onAuthFailure(error) {
+      console.log(error);
+    }
+  }
+}
+</script>
