0c81146672a7ca0b0e6adadc93044a3996060b16
John Brunton <jbrunton@zipcar.co.uk>
2020-04-26 23:32:06 +0100

    DEMO-20 add signed in details and sign out link

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,6 +14,27 @@
 
       <v-spacer></v-spacer>
 
+      <v-menu open-on-hover bottom offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn
+            text
+            v-on="on"
+          >
+            {{ signedInUser }}
+            <v-icon right>mdi-menu-down</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item href="#">
+            <v-list-item-title>Profile</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="signOut">
+            <v-list-item-title>Sign out</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
       <v-btn
         href="https://github.com/jbrunton/bechdel-demo"
         target="_blank"
@@ -37,3 +58,43 @@
     </v-content>
   </v-app>
 </template>
+<script>
+/* global gapi */
+export default {
+  data() {
+    return {
+      signedIn: false,
+      signedInUser: ""
+    }
+  },
+
+  mounted() {
+    gapi.load('auth2', this.checkAuthStatus);
+  },
+
+  methods : {
+    async checkAuthStatus() {
+      const auth = await gapi.auth2.init({
+          client_id: '952635847674-ocr6762iqhjkvtb988fclnfs4trr6qqr.apps.googleusercontent.com',
+          cookie_policy: 'single_host_origin',
+          scope: 'email profile',
+          response_type: 'code'
+      });
+      if (auth.isSignedIn.get()) {
+        this.signedIn = true;
+        this.signedInUser = auth.currentUser.get().getBasicProfile().getName()
+      } else {
+        this.signedIn = false;
+        this.signedInUser = "";
+      }
+    },
+
+    signOut() {
+      const auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(function () {
+        location.reload();
+      });
+    }
+  }
+}
+</script>
