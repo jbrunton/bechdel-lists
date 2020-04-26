95e7adf6b3c57492a5e67ff4a00231a5142d1155
John Brunton <jbrunton@zipcar.co.uk>
2020-04-26 11:10:46 +0100

    DEMO-12 add .sequelizerc file

--- api/db/index.js
+++ api/db/index.js
@@ -1,78 +1,78 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-// Option 1: Passing parameters separately
-const sequelize = new Sequelize('bechdel_demo', 'postgres', 'postgres_password', {
-  host: 'postgres',
-  dialect: 'postgres',
-  define: {
-    timestamps: true,
-    underscored: true
-  }
-});
+// // Option 1: Passing parameters separately
+// const sequelize = new Sequelize('bechdel_demo', 'postgres', 'postgres_password', {
+//   host: 'postgres',
+//   dialect: 'postgres',
+//   define: {
+//     timestamps: true,
+//     underscored: true
+//   }
+// });
 
-const List = sequelize.define('List', {
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true
-    }
-  },
-  averageRating: Sequelize.FLOAT,
-  description: Sequelize.STRING
-});
+// const List = sequelize.define('List', {
+//   title: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//     validate: {
+//       notEmpty: true
+//     }
+//   },
+//   averageRating: Sequelize.FLOAT,
+//   description: Sequelize.STRING
+// });
 
-List.prototype.updateDetails = async function() {
-  const movies = await this.getMovies();
+// List.prototype.updateDetails = async function() {
+//   const movies = await this.getMovies();
   
-  const ratings = movies.map((movie) => movie.rating).filter(x => x);
-  if (ratings.length > 0) {
-    this.averageRating = ratings.reduce((a, b) => a + b, 0) / ratings.length;
-  } else {
-    this.averageRating = null;
-  }
+//   const ratings = movies.map((movie) => movie.rating).filter(x => x);
+//   if (ratings.length > 0) {
+//     this.averageRating = ratings.reduce((a, b) => a + b, 0) / ratings.length;
+//   } else {
+//     this.averageRating = null;
+//   }
 
-  if (movies.length == 0) {
-    this.description = null;
-  } else if (movies.length == 1) {
-    this.description = movies[0].title;
-  } else if (movies.length == 2) {
-    this.description = `${movies[0].title} and ${movies[1].title}`;
-  } else {
-    const remainder = movies.length - 2;
-    const remainderDescription = remainder > 1 ? 'others' : 'other';
-    this.description = `${movies[0].title}, ${movies[1].title} and ${remainder} ${remainderDescription}`;
-  }
+//   if (movies.length == 0) {
+//     this.description = null;
+//   } else if (movies.length == 1) {
+//     this.description = movies[0].title;
+//   } else if (movies.length == 2) {
+//     this.description = `${movies[0].title} and ${movies[1].title}`;
+//   } else {
+//     const remainder = movies.length - 2;
+//     const remainderDescription = remainder > 1 ? 'others' : 'other';
+//     this.description = `${movies[0].title}, ${movies[1].title} and ${remainder} ${remainderDescription}`;
+//   }
 
-  await this.save();
-};
+//   await this.save();
+// };
 
-const Movie = sequelize.define('Movie', {
-  title: Sequelize.STRING,
-  imdbId: {
-    type: Sequelize.STRING,
-    unique: true
-  },
-  year: Sequelize.INTEGER,
-  rating: Sequelize.INTEGER
-});
+// const Movie = sequelize.define('Movie', {
+//   title: Sequelize.STRING,
+//   imdbId: {
+//     type: Sequelize.STRING,
+//     unique: true
+//   },
+//   year: Sequelize.INTEGER,
+//   rating: Sequelize.INTEGER
+// });
 
-Movie.belongsToMany(List, {through: 'ListEntry'});
-List.belongsToMany(Movie, {through: 'ListEntry'});
+// Movie.belongsToMany(List, {through: 'ListEntry'});
+// List.belongsToMany(Movie, {through: 'ListEntry'});
 
-module.exports = {
-  List: List,
-  Movie: Movie,
-  init: async () => {
-    try {
-      await sequelize.authenticate()
-      console.log('Connection has been established successfully.');
-    } catch (err) {
-      console.error('Unable to connect to the database:', err);
-    }
-    //await sequelize.sync({ force: true });
-    await sequelize.sync();
-  },
-  isValidationError: (error) => error instanceof Sequelize.ValidationError,
-  sequelize: sequelize
-}
+// module.exports = {
+//   List: List,
+//   Movie: Movie,
+//   init: async () => {
+//     try {
+//       await sequelize.authenticate()
+//       console.log('Connection has been established successfully.');
+//     } catch (err) {
+//       console.error('Unable to connect to the database:', err);
+//     }
+//     //await sequelize.sync({ force: true });
+//     await sequelize.sync();
+//   },
+//   isValidationError: (error) => error instanceof Sequelize.ValidationError,
+//   sequelize: sequelize
+// }
