62780f7c44c705a4c3ad41de4900ae42a4bb1942
John Brunton <jbrunton@zipcar.co.uk>
2020-04-27 15:15:02 +0100

    add express logging

--- api/config/app.js
+++ api/config/app.js
@@ -4,12 +4,14 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const morgan = require('morgan');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(session({
   secret: 'debug secret'
 }));
+app.use(morgan('dev'));
 
 console.log('Configuring routers...');
 glob.sync('./app/routers/*_router.js').forEach(function(file) {
