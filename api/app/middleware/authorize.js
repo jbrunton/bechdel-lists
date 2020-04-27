288b9578c8086e1e5f8e7894217d6f642a63e363
John Brunton <jbrunton@zipcar.co.uk>
2020-04-27 21:14:12 +0100

    DEMO-28 fix authorization

--- api/app/middleware/authorize.js
+++ api/app/middleware/authorize.js
@@ -4,7 +4,7 @@ module.exports = function(prototype) {
   return function(req, res, next) {
     if (prototype == models.List) {
       if (req.user) {
-        if (req.list && req.list.userId == req.user.id) {
+        if (req.list && req.list.UserId == req.user.id) {
           return next();
         }
       }
