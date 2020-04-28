25128236cfeb1ba61c2af7640102ad42de155a82
John Brunton <jbrunton@zipcar.co.uk>
2020-04-28 20:24:30 +0100

    DEMO-37 iterate over services

--- ci/build.sh
+++ ci/build.sh
@@ -5,11 +5,18 @@ export SHA=$(git rev-parse HEAD)
 
 echo "$DOCKER_ACCESS_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build \
- -t $DOCKER_USERNAME/bechdel-lists-api-dev:latest \
- -t $DOCKER_USERNAME/bechdel-lists-api-dev:$SHA \
- -f ./api/Dockerfile.dev ./api
+declare -a StringArray=("api" "client")
+ 
+# Iterate the string array using for loop
+for service in ${StringArray[@]}; do
+  echo "Building $service service..."
 
-docker push $DOCKER_USERNAME/bechdel-lists-api-dev:latest
+  docker build \
+    -t $DOCKER_USERNAME/bechdel-lists-$service-dev:latest \
+    -t $DOCKER_USERNAME/bechdel-lists-$service-dev:$SHA \
+    -f ./api/Dockerfile.dev ./$service
 
-docker push $DOCKER_USERNAME/bechdel-lists-api-dev:$SHA
+    docker push $DOCKER_USERNAME/bechdel-lists-$service-dev:latest
+
+    docker push $DOCKER_USERNAME/bechdel-lists-$service-dev:$SHA
+done
