a145c4248b13ea7a31a244313ea69ee5ec006541
John Brunton <jbrunton@zipcar.co.uk>
2020-04-29 20:47:58 +0100

    DEMO-48 pass postgres variables to deployment

--- api/config/database.js
+++ api/config/database.js
@@ -20,6 +20,7 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
     dialect: 'postgres',
     operatorsAliases: false
   }
