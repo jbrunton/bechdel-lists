e7f07fedde4f66977d69b954da14a14a1053f439
John Brunton <jbrunton@zipcar.co.uk>
2020-04-29 10:58:23 +0100

    DEMO-37 use docker-compose to build

--- ci/build.sh
+++ ci/build.sh
@@ -5,17 +5,5 @@ export SHA=$(git rev-parse HEAD)
 
 echo "$DOCKER_ACCESS_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-declare -a StringArray=("api" "client" "nginx")
- 
-# Iterate the string array using for loop
-for service in ${StringArray[@]}; do
-  echo "Building $service service..."
-
-  docker build \
-    -t $DOCKER_USERNAME/bechdel-lists-$service:latest \
-    -t $DOCKER_USERNAME/bechdel-lists-$service:$SHA \
-    -f ./$service/Dockerfile ./$service
-
-  docker push $DOCKER_USERNAME/bechdel-lists-$service:latest
-  docker push $DOCKER_USERNAME/bechdel-lists-$service:$SHA
-done
+docker-compose -f docker-compose.yml build
+docker-compose push
