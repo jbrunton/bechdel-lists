5791ad837dd7829d90726d25b561ab83793020b4
John Brunton <jbrunton@zipcar.co.uk>
2020-04-29 20:50:30 +0100

    DEMO-48 hardcode database name

--- api/config/database.js
+++ api/config/database.js
@@ -18,7 +18,7 @@ module.exports = {
   production: {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
+    database: 'bechdel_demo',
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     dialect: 'postgres',
