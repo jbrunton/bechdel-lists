6606302231a34a8afefec68736b68f1d14fd95b8
John Brunton <jbrunton@zipcar.co.uk>
2020-04-30 22:06:46 +0100

    DEMO-44 do a docker-compose pull

--- ci/generate_deployment.sh
+++ ci/generate_deployment.sh
@@ -7,6 +7,7 @@ echo "$DOCKER_ACCESS_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdi
 
 export DEPLOYMENT_FILE=deployments/docker-compose.${TAG}.yml
 docker-compose -v
+docker-compose pull
 docker-compose config --resolve-image-digests > $DEPLOYMENT_FILE
 
 echo "Generated deployment file $DEPLOYMENT_FILE:"
