30165d839cb1fad3a9c4bc946d159b362e1e5672
John Brunton <jbrunton@zipcar.co.uk>
2020-04-30 23:55:12 +0100

    DEMO-44 add some logging

--- ci/parse_tag.sh
+++ ci/parse_tag.sh
@@ -6,6 +6,9 @@ echo "TAG=$TAG"
 
 echo "Checking master contains tag $TAG"
 # This works because grep finishes with an exit code of 1 if it doesn't find anything
+git fetch
+echo "is shallow?"
+git rev-parse --is-shallow-repository
 git branch --contains tags/$TAG | grep master
 
 echo "::set-env name=TAG::$TAG"
