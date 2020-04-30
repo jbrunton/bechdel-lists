0c80882fc213e889ad6107351519b25106cfcc80
John Brunton <jbrunton@zipcar.co.uk>
2020-04-30 23:50:18 +0100

    DEMO-44 add logging

--- ci/parse_tag.sh
+++ ci/parse_tag.sh
@@ -4,7 +4,8 @@ set -e
 TAG=${GITHUB_REF/refs\/tags\//}
 echo "TAG=$TAG"
 
-# Verify the tag appears on master. Grep returns with an exit code of 1 if it doesn't find anything.
+echo "Checking master contains tag $TAG"
+# This works because grep finishes with an exit code of 1 if it doesn't find anything
 git branch --contains tags/$TAG | grep master
 
 echo "::set-env name=TAG::$TAG"
