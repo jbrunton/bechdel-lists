8fe6eae796c89f2b42f87177276d174fa4613e7b
John Brunton <jbrunton@zipcar.co.uk>
2020-04-30 22:46:52 +0100

    DEMO-44 add some logging

--- ci/generate_deployment.sh
+++ ci/generate_deployment.sh
@@ -17,3 +17,8 @@ git config --global user.email "jbrunton-ci-minion@outlook.com"
 git config --global user.name "jbrunton-ci-minion"
 git add $DEPLOYMENT_FILE
 git commit -m "Generated deployment file for $TAG"
+
+git status
+
+echo GITHUB_SHA=$GITHUB_SHA
+echo GITHUB_REF=$GITHUB_REF
