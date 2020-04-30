2d13836c292115196f36464862f4e88de96205de
John Brunton <jbrunton@zipcar.co.uk>
2020-04-30 19:07:05 +0100

    DEMO-44 add generate_deployment.sh

---
+++ ci/generate_deployment.sh
@@ -0,0 +1,6 @@
+export TAG=v0.0.1
+export DEPLOYMENT_FILE=deployments/docker-compose.${TAG}.yml
+docker-compose -f docker-compose.yml config --resolve-image-digests > $DEPLOYMENT_FILE
+
+echo "Generated deployment file $DEPLOYMENT_FILE:"
+cat $DEPLOYMENT_FILE
