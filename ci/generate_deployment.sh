8c7faffc40378e496eb490f81419f28b0f1ad8d5
John Brunton <jbrunton@zipcar.co.uk>
2020-04-30 19:21:35 +0100

    DEMO-44 add set -e

--- ci/generate_deployment.sh
+++ ci/generate_deployment.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 export TAG=v0.0.1
 export DEPLOYMENT_FILE=deployments/docker-compose.${TAG}.yml
 docker-compose -f docker-compose.yml config --resolve-image-digests > $DEPLOYMENT_FILE
