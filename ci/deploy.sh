f6b43b24f42ab0e67bf04c719dbb9f6a3f5c0a98
John Brunton <jbrunton@zipcar.co.uk>
2020-05-01 11:38:26 +0100

    DEMO-44 fix deploy script

--- ci/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "DEPLOYMENT_FILE: $DEPLOYMENT_FILE"
-export POSTGRES_CONNECTION
-docker-compose --file $DEPLOYMENT_FILE --project-directory . \
-  up --detach --no-build --remove-orphans
