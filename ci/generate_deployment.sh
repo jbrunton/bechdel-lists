89332a26d61b2a1389499de16e3eb0c2ed526b9d
John Brunton <jbrunton@zipcar.co.uk>
2020-05-01 09:57:05 +0100

    DEMO-44 fix order of git commands

--- ci/generate_deployment.sh
+++ ci/generate_deployment.sh
@@ -13,10 +13,10 @@ docker-compose config --resolve-image-digests > $DEPLOYMENT_FILE
 echo "Generated deployment file $DEPLOYMENT_FILE:"
 cat $DEPLOYMENT_FILE
 
+git add $DEPLOYMENT_FILE
 git stash save
 git checkout master
 git stash pop
-git add $DEPLOYMENT_FILE
 
 git config --global user.email "jbrunton-ci-minion@outlook.com"
 git config --global user.name "jbrunton-ci-minion"
