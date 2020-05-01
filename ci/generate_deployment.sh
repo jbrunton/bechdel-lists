273049582bbbdc86484187bd37e3989a6feb122d
John Brunton <jbrunton@zipcar.co.uk>
2020-05-01 11:31:23 +0100

    DEMO-44 export correct DEPLOYMENT_SHA

--- ci/generate_deployment.sh
+++ ci/generate_deployment.sh
@@ -12,19 +12,15 @@ docker-compose pull
 # docker-compose config resolves contexts to absolute paths, hence using sed to make them relative again
 docker-compose config --resolve-image-digests | sed "s#$(pwd)/##" > $DEPLOYMENT_FILE
 
-echo "Generated deployment file $DEPLOYMENT_FILE:"
-cat $DEPLOYMENT_FILE
-
 git add $DEPLOYMENT_FILE
 git config --global user.email "jbrunton-ci-minion@outlook.com"
 git config --global user.name "jbrunton-ci-minion"
 git commit -m "Generated deployment file for $TAG"
 
-git status
-
-echo GITHUB_SHA=$GITHUB_SHA
-echo GITHUB_REF=$GITHUB_REF
+echo "Generated deployment file $DEPLOYMENT_FILE:"
+cat $DEPLOYMENT_FILE
 
 git push origin HEAD:master
 
 echo "::set-env name=DEPLOYMENT_FILE::$DEPLOYMENT_FILE"
+echo "::set-env name=DEPLOYMENT_SHA::$(git rev-parse HEAD)"
