7936768321b71e3602b8c258e3d4cba08de730fc
John Brunton <jbrunton@zipcar.co.uk>
2020-05-02 11:15:00 +0100

    DEMO-59 try using outputs

--- ci/check_manifest.sh
+++ ci/check_manifest.sh
@@ -4,5 +4,5 @@ set -e
 npm run deploy:check -- --output-file check.env
 source check.env
 
-echo "::set-env name=DEPLOYMENT_REQUIRED::$DEPLOYMENT_REQUIRED"
-echo "::set-env name=BUILD_IDS::$BUILD_IDS"
+echo "::set-output name=deploymentRequired::$DEPLOYMENT_REQUIRED"
+echo "::set-output name=buildIds::$BUILD_IDS"
