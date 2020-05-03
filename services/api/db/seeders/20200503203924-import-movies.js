e5265a7a4f42d5c90621a09e3917016e5f9995a3
jbrunton <john_brunton@hotmail.co.uk>
2020-05-03 22:54:25 +0100

    DEMO-67 reimplement seed users and lists to work with import-movies

--- services/api/db/seeders/20200503203924-import-movies.js
+++ services/api/db/seeders/20200503203924-import-movies.js
@@ -20,12 +20,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    await queryInterface.bulkDelete('Movies', null, {});
   }
 };
---
+++ services/api/db/seeders/20200503212421-test-lists.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const models = require('../../models');
+const yaml = require('js-yaml');
+const fs   = require('fs');
+
+const demoUserDetails = {
+  email: process.env.SEED_USER_EMAIL || 'demo.user@example.com',
+  userName: process.env.SEED_USER_NAME || 'Demo User'
+};
+
+const testUserDetails = {
+  email: 'test.user@example.com',
+  name: 'Test User'
+};
+
+async function createUser(details) {
+  return await models.User.findOrCreateByEmail(details.email, details.name);
+}
+
+async function createLists(lists, user) {
+  for (const listSeed of lists) {
+    const list = await models.List.create({
+      title: listSeed.title,
+      UserId: user.id
+    });
+
+    for (const movieSeed of listSeed.movies) {
+      const movie = await models.Movie.findOne({ where: { imdbId: movieSeed.imdbId } });
+      if (!movie) {
+        throw new Error(`Could not find ${movieSeed.title}. Make sure you run the import-movies migration first`);
+      }
+      await list.addMovie(movie)
+      await list.updateDetails();
+    }
+  }
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    try {
+      const seeds = yaml.safeLoad(fs.readFileSync('./db/seeders/lists.yml', 'utf8'));
+      
+      const demoUser = await createUser(demoUserDetails);
+      await createLists(seeds.demoUserLists, demoUser);
+
+      const testUser = await createUser(testUserDetails);
+      await createLists(seeds.testUserLists, testUser);
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('ListEntries', null, {});
+    await queryInterface.bulkDelete('Lists', null, {});
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
