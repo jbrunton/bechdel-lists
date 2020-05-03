99b29ab7d7c91bb62b4ae73305ea8f06c95cc43e
jbrunton <john_brunton@hotmail.co.uk>
2020-05-03 13:52:08 +0100

    DEMO-61 add ci:deploy:commit file

--- tasks/lib/deployments.js
+++ tasks/lib/deployments.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
+const { v4: uuid } = require('uuid');
+const logger = require('../lib/logger');
 
 class Deployments {
   constructor(environment) {
@@ -8,7 +10,8 @@ class Deployments {
   }
 
   loadManifest() {
-    this.manifest = yaml.safeLoad(fs.readFileSync(`./deployments/${this.environment}.yml`, 'utf8'));
+    this.manifestFile = `./deployments/${this.environment}.yml`;
+    this.manifest = yaml.safeLoad(fs.readFileSync(this.manifestFile, 'utf8'));
 
     this._deploymentsById = [];
     for (let deployment of this.manifest.deployments) {
@@ -18,6 +21,26 @@ class Deployments {
 
   getLatest() {
     return this._deploymentsById[this.manifest.latest];
+  }
+
+  create(version, dryRun) {
+    const deployment = {
+      id: uuid(),
+      version: version,
+      timestamp: new Date().toISOString()
+    };
+    
+    this.manifest.deployments.push(deployment);
+    if (!dryRun) {
+      fs.writeFileSync(this.manifestFile, yaml.safeDump(this.manifest));
+      console.log('Added build to catalog.');
+    } else {
+      logger.info('--dry-run passed, skipping adding deployment to manifest');
+      logger.info('Would have added:');
+      logger.infoBlock(yaml.safeDump(deployment));
+    }
+
+    return deployment;
   }
 }
 
