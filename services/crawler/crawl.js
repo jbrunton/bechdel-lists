f5f15cd001935106451624262493a004e100351b
jbrunton <john_brunton@hotmail.co.uk>
2020-05-04 16:35:40 +0100

    DEMO-77 scrape box office info

--- services/crawler/crawl.js
+++ services/crawler/crawl.js
@@ -1,18 +1,53 @@
 const puppeteer = require('puppeteer');
+const argv = require('yargs').argv;
+
+const listUrl = argv.list;
+if (!listUrl) {
+  throw Error("Required --list url to be given.");
+}
+
+process.on('unhandledRejection', error => {
+  console.log(error);
+  process.exit(1);
+});
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.boxofficemojo.com/year/2019/');
+  await page.goto(listUrl);
 
-  const title = await page.title();
-  console.log(`Title: ${title}`);
-  const links = await page.$$eval("a[href*='/release/rl']", links => Array.from(links).map((x) => x.getAttribute('href')));
-  //const releases = Array.from(links).map((x) => x.getAttribute('href'))
-  //Array.from(document.querySelectorAll("a[href*='/release/rl']")).map((x) => x.getAttribute('href'))
-  console.log(`LINKS: ${links}`);
-  const releaseIds = links.map(link => link.match(/release\/(rl\d+)/)[1]);
+  const top100links = (await page.$$eval("a[href*='/release/rl']", links => {
+    return Array.from(links).map(link => link.getAttribute('href'))
+  })).slice(0, 5);
+
+  const releaseIdRegex = /release\/(rl\d+)/
+  const releaseIds = top100links.map(link => link.match(releaseIdRegex)[1]);
   console.log(releaseIds);
+
+  const summaryHeadingSelector = '.mojo-performance-summary-table .a-section span.a-size-small';
+  const summaryValuesSelector = '.mojo-performance-summary-table .a-section span.money'
+
+  for (let releaseId of releaseIds) {
+    const releaseUrl = `https://www.boxofficemojo.com/release/${releaseId}`;
+    console.log(`Fetching release ${releaseId} from ${releaseUrl}`);
+    await page.goto(releaseUrl);
+
+    const title = await page.$eval('h1', heading => heading.innerText);
+    console.log('title: ' + title);
+    const summaryHeadings = await page.$$eval(summaryHeadingSelector, headings => {
+      return Array.from(headings).map(heading => heading.innerText)
+    });
+    console.log('summaryHeadings: ' + summaryHeadings);
+    const summaryValues = await page.$$eval(summaryValuesSelector, headings => {
+      return Array.from(headings).map(heading => heading.innerText)
+    });
+    console.log('summaryValues: ' + summaryValues);
+    const domesticIndex = summaryHeadings.findIndex(heading => heading.toLowerCase().includes('domestic'));
+    const globalIndex = summaryHeadings.findIndex(heading => heading.toLowerCase().includes('worldwide'));
+    const domesticBoxOffice = summaryValues[domesticIndex];
+    const globalBoxOffice = summaryValues[globalIndex];
+    console.log(`${title} - domestic: ${domesticBoxOffice}, global: ${globalBoxOffice}`);
+  }
 
   await browser.close();
 })();
