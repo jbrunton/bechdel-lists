1fda080be576ff60367908ef03c7bf6f58ec78c6
jbrunton <john_brunton@hotmail.co.uk>
2020-05-04 15:53:22 +0100

    DEMO-77 use puppeteer

--- services/crawler/crawl.js
+++ services/crawler/crawl.js
@@ -1,68 +1,18 @@
-const Apify = require('apify');
+const puppeteer = require('puppeteer');
 
-Apify.main(async () => {
-    const requestQueue = await Apify.openRequestQueue();
-    await requestQueue.addRequest({ url: 'https://www.boxofficemojo.com/year/2019/' });
-    //const pseudoUrls = [new Apify.PseudoUrl('https://www.iana.org/[.*]')];
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.boxofficemojo.com/year/2019/');
 
-    const crawler = new Apify.PuppeteerCrawler({
-        requestQueue,
-        headless: true,
-        handlePageFunction: async ({ request, page }) => {
-            const title = await page.title();
-            console.log(`Title of ${request.url}: ${title}`);
-            const links = await page.$$eval("a[href*='/release/rl']", links => Array.from(links).map((x) => x.getAttribute('href')));
-            //const releases = Array.from(links).map((x) => x.getAttribute('href'))
-            //Array.from(document.querySelectorAll("a[href*='/release/rl']")).map((x) => x.getAttribute('href'))
-            console.log(`LINKS: ${links}`);
-            const releaseIds = links.map(link => link.match(/release\/(rl\d+)/)[1]);
-            console.log(releaseIds);
-                // await Apify.utils.enqueueLinks({
-                //     page,
-                //     selector: 'a',
-                //     pseudoUrls,
-                //     requestQueue,
-                // });
-        },
-        maxRequestsPerCrawl: 100,
-        maxConcurrency: 10,
-    });
+  const title = await page.title();
+  console.log(`Title: ${title}`);
+  const links = await page.$$eval("a[href*='/release/rl']", links => Array.from(links).map((x) => x.getAttribute('href')));
+  //const releases = Array.from(links).map((x) => x.getAttribute('href'))
+  //Array.from(document.querySelectorAll("a[href*='/release/rl']")).map((x) => x.getAttribute('href'))
+  console.log(`LINKS: ${links}`);
+  const releaseIds = links.map(link => link.match(/release\/(rl\d+)/)[1]);
+  console.log(releaseIds);
 
-    await crawler.run({ headless: true });
-});
-
-// const crawler = new Apify.PuppeteerCrawler({
-//   headless: true,
-//   requestList,
-//   handlePageFunction: async ({ page, request }) => {
-//       // This function is called to extract data from a single web page
-//       // 'page' is an instance of Puppeteer.Page with page.goto(request.url) already called
-//       // 'request' is an instance of Request class with information about the page to load
-//       await Apify.pushData({
-//           title: await page.title(),
-//           url: request.url,
-//           succeeded: true,
-//       });
-//   },
-//   handleFailedRequestFunction: async ({ request }) => {
-//       // This function is called when the crawling of a request failed too many times
-//       await Apify.pushData({
-//           url: request.url,
-//           succeeded: false,
-//           errors: request.errorMessages,
-//       });
-//   },
-// });
-
-// await crawler.run();
-
-// const puppeteer = require('puppeteer');
-
-// (async () => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto('https://example.com');
-//   await page.screenshot({path: 'example.png'});
-
-//   await browser.close();
-// })();
+  await browser.close();
+})();
