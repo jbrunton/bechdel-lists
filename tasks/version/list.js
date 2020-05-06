9752ebd541e12f43843d58843481bce9cbdb2279
jbrunton <john_brunton@hotmail.co.uk>
2020-05-07 00:23:49 +0100

    DEMO-84 load manifest from github

--- tasks/version/list.js
+++ tasks/version/list.js
@@ -1,5 +1,7 @@
+const axios = require('axios');
+const yaml = require('js-yaml');
+
 const logger = require('../lib/logger');
-const builds = require('../lib/builds');
 const args = require('../lib/args');
 
 const MAX_BUILDS = 10;
@@ -9,13 +11,21 @@ if (!showAll) {
   logger.info(`Showing ${MAX_BUILDS} most recent builds. Pass --all to show all.`);
 }
 
-const sortedBuilds = [...builds.catalog.builds].sort(build => -Date.parse(build.timestamp));
-const buildsToShow = showAll ? sortedBuilds : sortedBuilds.slice(0, MAX_BUILDS);
-const buildInfo = buildsToShow.map(build => {
-  return {
-    version: build.version,
-    time: new Date(build.timestamp).toLocaleString(),
-    commit: build.buildSha
-  };
-});
-console.table(buildInfo);
+async function listVersions() {
+  const catalogPath = 'https://raw.githubusercontent.com/jbrunton/bechdel-demo/master/deployments/builds/catalog.yml';
+  const catalogFile = await axios.get(catalogPath);
+  const catalog = yaml.safeLoad(catalogFile.data);
+
+  const sortedBuilds = [...(catalog.builds)].sort(build => Date.parse(build.timestamp));
+  const buildsToShow = showAll ? sortedBuilds : sortedBuilds.slice(0, MAX_BUILDS);
+  const buildInfo = buildsToShow.map(build => {
+    return {
+      version: build.version,
+      time: new Date(build.timestamp).toLocaleString(),
+      commit: build.buildSha
+    };
+  });
+  console.table(buildInfo);
+}
+
+listVersions();
