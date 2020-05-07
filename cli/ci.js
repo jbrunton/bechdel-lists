9a7deb4b87b71cb17e2b4d87c5a0904306e7d8c7
jbrunton <john_brunton@hotmail.co.uk>
2020-05-07 17:28:02 +0100

    DEMO-90 implement cli generate commands

--- cli/ci.js
+++ cli/ci.js
@@ -23,7 +23,44 @@ sywac.command('check manifest', {
     console.log(`::set-output name=buildMatrix::${JSON.stringify(buildMatrix)}}"`);
     console.log(`::set-output name=deploymentMatrix::${JSON.stringify(deploymentMatrix)}}"`);
   }
-})
+});
+
+sywac.command('generate <subcommand> [args]', {
+  ignore: ['<subcommand>', '[args]'],
+  setup: sywac => {
+    sywac
+      .command('build-payload <version>', {
+        desc: 'Generate a deployment payload to create a build for <version>',
+        run: (argv, context) => {
+          const payload = {
+            ref: process.env.GITHUB_REF || 'master',
+            environment: 'build',
+            task: 'build',
+            description: 'Trigger build',
+            payload: {
+              buildVersion: argv.version
+            }
+          };
+          console.log(`::set-output name=payload::${JSON.stringify(payload)}`);
+        }
+      })
+      .command('deploy-payload <version> <environment>', {
+        desc: 'Generate a deployment payload to deploy <version> to <environment>',
+        run: (argv, context) => {
+          const payload = {
+            ref: process.env.GITHUB_REF || 'master',
+            environment: argv.environment,
+            task: 'deploy',
+            description: 'Trigger deployment',
+            payload: {
+              buildVersion: argv.version
+            }
+          };
+          console.log(`::set-output name=payload::${JSON.stringify(payload)}`);
+        }
+      });
+  }
+});
 
 sywac.showHelpByDefault();
 
