42974048921d957fa7cc79c694d58bf81f386847
jbrunton <john_brunton@hotmail.co.uk>
2020-05-08 23:17:06 +0100

    DEMO-95 extract inspect command

--- cli/cli.js
+++ cli/cli.js
@@ -1,44 +1,9 @@
 #!/usr/bin/env node
 
 const sywac = require('sywac');
-const axios = require('axios');
-const yaml = require('js-yaml');
-const colors = require('colors/safe');
-
-const { exec } = require('child_process');
-const chalk = require('chalk');
-
 
 sywac
-  .command('info <environment>', {
-    desc: 'Display information from the manifest',
-    run: async (argv, context) => {
-      const manifest = await fetchManifest();
-      const environment = manifest.environments[argv.environment];
-      if (!environment) {
-        throw new Error(`Unknown environment ${argv.environment}`);
-      }
-
-      console.log(colors.bold('Environment info'));
-      const deployments = await fetchDeployments(argv.environment);
-      console.table({
-        version: environment.version,
-        host: environment.host
-      });
-
-      console.log(colors.bold('Recent deployments'));
-      console.table(deployments.deployments.slice(0, 5).map(deployment => {
-        return {
-          version: deployment.version,
-          timestamp: formatTimestamp(deployment.timestamp),
-          id: deployment.id
-        };
-      }));    
-    }
-  })
-  .command(require('./cli/list'))
-  .command(require('./cli/new_version'))
-  .command(require('./cli/deploy'))
+  .commandDirectory('./cli')
   .showHelpByDefault();
 
   sywac.style(require('./lib/style'));
---
+++ cli/cli/inspect.js
@@ -0,0 +1,30 @@
+const { fetchManifest, fetchDeployments, formatTimestamp } = require('../lib/utils');
+const chalk = require('chalk');
+
+module.exports = {
+  flags: 'inspect <environment>',
+  desc: 'Display information about the environment',
+  run: async (argv, context) => {
+    const manifest = await fetchManifest();
+    const environment = manifest.environments[argv.environment];
+    if (!environment) {
+      throw new Error(`Unknown environment ${argv.environment}`);
+    }
+
+    console.log(chalk.bold('Environment info'));
+    const deployments = await fetchDeployments(argv.environment);
+    console.table({
+      version: environment.version,
+      host: environment.host
+    });
+
+    console.log(chalk.bold('Recent deployments'));
+    console.table(deployments.deployments.slice(0, 5).map(deployment => {
+      return {
+        version: deployment.version,
+        timestamp: formatTimestamp(deployment.timestamp),
+        id: deployment.id
+      };
+    }));    
+  }
+}
