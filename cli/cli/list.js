7235064ec769dfa1dfd15d40a4adf7258529dfb0
jbrunton <john_brunton@hotmail.co.uk>
2020-05-09 11:23:47 +0100

    DEMO-95 add cli-table 3

--- cli/cli/list.js
+++ cli/cli/list.js
@@ -1,4 +1,38 @@
+const { table } = require('table');
+const Table = require('cli-table3');
+const chalk = require('chalk');
 const { formatTimestamp, fetchBuilds, fetchDeployments, fetchManifest } = require('../lib/utils');
+
+const defaultConfig = {
+  drawHorizontalLine: (index, size) => [0, 1, size].includes(index),
+  columnDefault: {
+    paddingLeft: 2,
+    paddingRight: 2
+  }
+};
+
+const defaultStyles = {
+  compact: true,
+  head: [],
+  paddingLeft: 2,
+  paddingRight: 2,
+  border: ['black']
+};
+
+function printTable(data, styles, options) {
+  const header = Object.keys(data[0]).map(s => ({ hAlign: 'center', content: s }));
+  const rows = data.map(x => {
+    return Object.values(x).map((v, index) => {
+      return index == 0 ? chalk.green(v) : chalk.yellow(v);
+    });
+  });
+  const table = new Table(Object.assign({
+    head: header,
+    style: Object.assign(defaultStyles, styles)
+  }, options));
+  table.push(...rows);
+  console.log(table.toString());
+}
 
 module.exports = {
   flags: 'list <builds|deployments|environments> [args]',
@@ -17,7 +51,7 @@ module.exports = {
               timestamp: formatTimestamp(build.timestamp),
             }
           });
-          console.table(builds);
+          printTable(builds, null, { colAligns: ['right'] });
         }
       })
       .command('deployments <environment>', {
