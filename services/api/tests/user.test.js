8f26d37936a6969e579cd3fa56bf584c98a03d0b
jbrunton <john_brunton@hotmail.co.uk>
2020-05-09 16:05:42 +0100

    DEMO-98 add simple user test

--- services/api/tests/user.test.js
+++ services/api/tests/user.test.js
@@ -1,3 +1,13 @@
-test('true', () => {
-  expect(true).toBe(false);
+const models = require('../models');
+
+const User = models.User;
+
+describe('User', () => {
+  test('excludingPII', () => {
+    const user = User.build({
+      name: 'My Name',
+      email: 'my.email@example.com'
+    });
+    expect(user.getExcludingPII()).toStrictEqual({ id: null });  
+  })
 });
