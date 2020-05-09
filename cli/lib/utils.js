9791da1219a16396b9424216360928dc0b0e4b9e
jbrunton <john_brunton@hotmail.co.uk>
2020-05-09 13:14:05 +0100

    DEMO-96 replace usage of utils fetch methods

--- cli/lib/utils.js
+++ cli/lib/utils.js
@@ -5,10 +5,7 @@ const chalk = require('chalk');
 
 module.exports = {
   formatTimestamp: formatTimestamp,
-  formatTable: formatTable,
-  fetchManifest: fetchManifest,
-  fetchBuilds: fetchBuilds,
-  fetchDeployments: fetchDeployments
+  formatTable: formatTable
 };
 
 function formatTimestamp(timestamp) {
@@ -51,25 +48,4 @@ function formatTable(data, styles, options) {
   }, options));
   table.push(...rows);
   return table.toString();
-}
-
-async function fetchManifest() {
-  const manifestUrl = 'https://raw.githubusercontent.com/jbrunton/bechdel-demo/master/manifest.yml';
-  const manifestFile = await axios.get(manifestUrl);
-  const manifest = yaml.safeLoad(manifestFile.data);
-  return manifest;
-}
-
-async function fetchDeployments(environment) {
-  const manifestUrl = `https://raw.githubusercontent.com/jbrunton/bechdel-demo/master/deployments/${environment}.yml`;
-  const manifestFile = await axios.get(manifestUrl);
-  const manifest = yaml.safeLoad(manifestFile.data);
-  return manifest;
-}
-
-async function fetchBuilds() {
-  const manifestUrl = `https://raw.githubusercontent.com/jbrunton/bechdel-demo/master/deployments/builds/catalog.yml`;
-  const manifestFile = await axios.get(manifestUrl);
-  const manifest = yaml.safeLoad(manifestFile.data);
-  return manifest;
 }
