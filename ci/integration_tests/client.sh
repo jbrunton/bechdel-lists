cc4f5da58e12a081aa4bc5d2e653a39d7cc4e5ad
jbrunton <john_brunton@hotmail.co.uk>
2020-05-10 14:32:16 +0100

    DEMO-100 refactor integration scripts

--- ci/integration_tests/client.sh
+++ ci/integration_tests/client.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-export CLIENT_DIR=${WORKSPACE}/services/client
+export CLIENT_DIR=$WORKSPACE/services/client
+
 cd $CLIENT_DIR
 npm install
+
+cd $WORKSPACE
+docker-compose up -d
 docker run -v $CLIENT_DIR:/e2e -w /e2e cypress/included:4.5.0
