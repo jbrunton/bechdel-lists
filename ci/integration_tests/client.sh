a23df56ed1371912e0f2373620126eaf12244024
jbrunton <john_brunton@hotmail.co.uk>
2020-05-10 13:57:53 +0100

    DEMO-100 use more up to date version of cypress/included

--- ci/integration_tests/client.sh
+++ ci/integration_tests/client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker run -v ${WORKSPACE}/services/client/:/e2e -w /e2e cypress/included:3.3.1
+docker run -v ${WORKSPACE}/services/client/:/e2e -w /e2e cypress/included:4.5.0
