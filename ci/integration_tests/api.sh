91e4a8e09b52fcd92a3809eae7325110771a23f4
jbrunton <john_brunton@hotmail.co.uk>
2020-05-11 09:51:11 +0100

    DEMO-101 experiment with caching

--- ci/integration_tests/api.sh
+++ ci/integration_tests/api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker-compose up -d
+docker-compose up -d --no-build
 docker-compose run api npm run db:test:create
 docker-compose run api npm run test:integration
           
