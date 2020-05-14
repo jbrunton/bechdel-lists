c962783ee7539fda2b11444a5e72ccdf2cfe7a17
jbrunton <john_brunton@hotmail.co.uk>
2020-05-14 12:59:58 +0100

    DEMO-119 import db.yml

--- services/api/db/seeders/20200503203924-import-movies.js
+++ services/api/db/seeders/20200503203924-import-movies.js
@@ -1,18 +1,30 @@
 'use strict';
 
 const fs = require('fs');
+const yaml = require('js-yaml');
+const models = require('../../models');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const seedMovies = JSON.parse(fs.readFileSync('./db/seeders/movies.json', 'utf8'));
+    const db = yaml.safeLoad(fs.readFileSync('./db/seeders/db.yml', 'utf8'));
+    const movieGenres = {};
     const date = new Date();
-    for (let movie of seedMovies) {
-      movie.imdbId = movie.imdbid;
-      delete movie.imdbid;
+    for (let movie of db.movies) {
       movie.createdAt = movie.updatedAt = date;
+      movieGenres[movie.imdbId] = movie.genres;
+      delete movie.genres;
+    }
+    for (let genre of db.genres) {
+      genre.createdAt = genre.updatedAt = date;
     }
     try {
-      await queryInterface.bulkInsert('Movies', seedMovies, {});
+      await queryInterface.bulkInsert('Movies', db.movies, {});
+      await queryInterface.bulkInsert('Genres', db.genres, {});
+      for (let [imdbId, genreIds] of Object.entries(movieGenres)) {
+        const movie = await models.Movie.findOne({ where: { imdbId: imdbId } });
+        const genres = await models.Genre.findAll({ where: { tmdbId: genreIds } });
+        await movie.addGenres(genres);
+      }
     } catch(e) {
       console.log(e);
       process.exit(1);
@@ -21,5 +33,6 @@ module.exports = {
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Movies', null, {});
+    await queryInterface.bulkDelete('Genres', null, {});
   }
 };
