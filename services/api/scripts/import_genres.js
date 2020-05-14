a9a49e905f8fb9b1867806a402b6d831bf8f53cc
jbrunton <john_brunton@hotmail.co.uk>
2020-05-14 11:15:05 +0100

    DEMO-119 fetch genre ids for movies

--- services/api/scripts/import_genres.js
+++ services/api/scripts/import_genres.js
@@ -8,7 +8,7 @@ if (!apiKey) {
 }
 const moviedb = new MovieDb(apiKey);
 
-async function main() {
+async function importGenres() {
   const response = await moviedb.genreMovieList();
   for (let genreData of response.genres) {
     const [genre, created] = await models.Genre.findOrCreate({
@@ -22,6 +22,20 @@ async function main() {
       console.log('Found existing genre: ' + JSON.stringify(genre.get()));
     }
   }
+}
+
+async function updateGenres() {
+  const movies = (await models.Movie.findAll()).slice(0, 10);
+  for (let movie of movies) {
+    console.log('imdbId: ' + movie.imdbId);
+    const externalData = await moviedb.find({ id: `tt${movie.imdbId}`, external_source: 'imdb_id' });
+    console.log('genre info for ' + movie.title + ' : ' + JSON.stringify(externalData.movie_results[0].genre_ids));
+  }
+}
+
+async function main() {
+  //await importGenres();
+  await updateGenres();
 }
 
 (async function() {
