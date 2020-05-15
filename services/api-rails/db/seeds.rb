1e326ec5d884ec8495fee0db30cd5e64c7557efe
jbrunton <john_brunton@hotmail.co.uk>
2020-05-15 16:28:00 +0100

    DEMO-127 import movies from seeds file

--- services/api-rails/db/seeds.rb
+++ services/api-rails/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'yaml'
+
+db = YAML.load(File.read('./db/seeds/db.yml'))
+movie_genres = {}
+date = Time.now
+db['movies'].each do |movie|
+  movie['created_at'] = movie['updated_at'] = date
+  movie_genres[movie['id']] = movie['genre_ids'];
+  movie.delete('genre_ids')
+end
+Movie.insert_all db['movies']
