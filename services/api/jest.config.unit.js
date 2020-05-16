ca39b73db0e206212fdae8e49b81a779b2bb220c
jbrunton <john_brunton@hotmail.co.uk>
2020-05-16 21:23:43 +0100

    DEMO-133 remove api service

--- services/api/jest.config.unit.js
+++
@@ -1,7 +0,0 @@
-const defaults = require('./jest.config');
-
-module.exports = Object.assign({
-  testMatch: [
-    "**/tests/unit/**/*.js?(x)"
-  ]
-}, defaults);
--- services/api/tests/suite_setup.js
+++
@@ -1,2 +0,0 @@
-const sequelize = require('../models').sequelize;
-afterAll(() => sequelize.close())
