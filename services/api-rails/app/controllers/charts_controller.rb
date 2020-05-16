695fd56d7d62cda360827425861e3fa9472f6b13
jbrunton <john_brunton@hotmail.co.uk>
2020-05-16 13:31:43 +0100

    DEMO-127 refactor loops

--- services/api-rails/app/controllers/charts_controller.rb
+++ services/api-rails/app/controllers/charts_controller.rb
@@ -8,10 +8,8 @@ class ChartsController < ApplicationController
     ratings_data = [%w(Year 0 1 2 3)]
     average_data = [%w(Year Average)]
 
-    min_year = @results.map { |result| result.year }.min
-    max_year = @results.map { |result| result.year }.max
-
-    for year in min_year..max_year
+    years = @results.pluck(:year)
+    (years.min..years.max).each do |year|
       ratings = get_ratings_for_year(@results, year)
       ratings_data << [year.to_s] + ratings
       average_data << build_average_row(ratings, year)
@@ -26,12 +24,11 @@ class ChartsController < ApplicationController
   private
 
   def get_ratings_for_year(results, year)
-    ratings = []
-    for rating in 0..3
-      result = results.find { |x| x[:year] == year && x[:rating] == rating }
-      ratings << (result.try(:[], :count) || 0)
+    (0..3).map do |rating|
+      results
+          .find { |x| x[:year] == year && x[:rating] == rating }
+          .try(:[], :count) || 0
     end
-    ratings
   end
 
   def build_average_row(ratings, year)
