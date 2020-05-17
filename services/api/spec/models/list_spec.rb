8835052490ad80269fd07c2bec8dea20af4343b6
jbrunton <john_brunton@hotmail.co.uk>
2020-05-17 13:37:46 +0100

    DEMO-129 add failing unit test for List

--- services/api/spec/models/list_spec.rb
+++ services/api/spec/models/list_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".stats_for" do
+    it "calculates descriptive statistics for the given list of movies" do
+      movies = [Movie.create(title: 'Movie 1', rating: 1), Movie.create(title: 'Movie 2', rating: 3)]
+      stats = List.stats_for(movies)
+      expect(stats).to eq(min_rating: 1, max_rating: 3, average_rating: 1)
+    end
+  end
 end
