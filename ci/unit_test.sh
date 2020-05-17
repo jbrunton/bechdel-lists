af59eb1726bd2718f54c232527b89942c5c71bf0
jbrunton <john_brunton@hotmail.co.uk>
2020-05-17 14:08:43 +0100

    DEMO-129 run api integration tests

--- ci/unit_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-cd ${WORKSPACE}/services/${SERVICE}
-npm install
-npm run test:unit
--- ci/unit_tests/api.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-cd $WORKSPACE
-docker-compose run api bin/rails db:test:prepare
-docker-compose run api bin/rails spec
