aa6928b3b37324c2f1b5d2922c272474da1db368
jbrunton <john_brunton@hotmail.co.uk>
2020-05-18 17:02:46 +0100

    DEMO-134 run npm install

--- ci/integration_tests/client.sh
+++ ci/integration_tests/client.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+cd ${WORKSPACE}/services/client
+npm install
+
+cd ${WORKSPACE}
 docker-compose up -d client
 docker-compose run api bin/rails db:migrate RAILS_ENV=development
 docker-compose run cypress
