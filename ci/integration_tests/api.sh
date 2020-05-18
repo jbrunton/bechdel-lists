b8a88abf00c6aca1b60ee1ba14d03b343f901411
jbrunton <john_brunton@hotmail.co.uk>
2020-05-18 16:20:32 +0100

    DEMO-134 fix integration test scripts

--- ci/integration_tests/api.sh
+++ ci/integration_tests/api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-docker-compose up api -d
+docker-compose up -d api
 docker-compose run api bin/rails db:test:prepare
 docker-compose run api bin/rails spec:integration
--- ci/integration_tests/client.sh
+++ ci/integration_tests/client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker-compose cypress run
+docker-compose run cypress
