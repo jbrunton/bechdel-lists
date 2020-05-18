52cd2c21cde75421552931bcf3f8cac409d6501d
jbrunton <john_brunton@hotmail.co.uk>
2020-05-18 21:00:35 +0100

    DEMO-135 extract docker-compose.cypress.yml

--- ci/integration_tests/client.sh
+++ ci/integration_tests/client.sh
@@ -5,6 +5,7 @@ cd ${WORKSPACE}/services/client
 npm install
 
 cd ${WORKSPACE}
-docker-compose up -d client
+docker-compose up -d
 docker-compose run api bin/rails db:migrate RAILS_ENV=development
+export COMPOSE_FILE=docker-compose.yml:docker-compose.override.yml:docker-compose.cypress.yml
 docker-compose run cypress
