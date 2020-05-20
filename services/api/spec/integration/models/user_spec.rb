9af04ba02860f3330d8210262a760b5828a2b72b
jbrunton <john_brunton@hotmail.co.uk>
2020-05-20 10:53:36 +0100

    DEMO-140 add test for find_or_create_by_email

---
+++ services/api/spec/integration/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe ".find_or_create_by_email" do
+    context "if the user doesn't exist" do
+      it "creates a new user with the given name and email" do
+        user = User.find_or_create_by_email('test.user@example.com', 'Test User')
+
+        expect(user.email).to eq('test.user@example.com')
+        expect(user.display_name).to eq('Test User')
+      end
+    end
+
+    context "if the user exists" do
+      let!(:user) { User.create(email: 'test.user@example.com', display_name: 'Display Name' )}
+
+      it "finds the existing user" do
+        found_user = User.find_or_create_by_email('test.user@example.com', 'Test User')
+        expect(found_user).to eq(user)
+      end
+
+      it "keeps the existing display_name" do
+        found_user = User.find_or_create_by_email('test.user@example.com', 'Test User')
+        expect(found_user.display_name).to eq('Display Name')
+      end
+    end
+  end
+end
