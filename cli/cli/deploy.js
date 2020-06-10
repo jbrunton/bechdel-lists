e80d43dedf38515f1bebe82b469d53c105b974fd
jbrunton <john_brunton@hotmail.co.uk>
2020-06-10 16:57:24 +0100

    LIST-197 target list-197

--- cli/cli/deploy.js
+++ cli/cli/deploy.js
@@ -17,7 +17,7 @@ module.exports = {
     console.log(`currentVersion: ${currentVersion}, nextVersion: ${nextVersion}`);
 
     const payload = {
-      ref: 'master',
+      ref: 'list-197',
       environment: 'update_manifest',
       task: 'update_manifest',
       description: 'New version',
