98be1809705122ff19718bf31cc0557750abbfa3
jbrunton <john_brunton@hotmail.co.uk>
2020-06-10 11:24:30 +0100

    LIST-196 improve logging

--- cli/cli.js
+++ cli/cli.js
@@ -3,15 +3,20 @@
 const sywac = require('sywac');
 const { TypeEnvironment, TypeVersion, TypeService, TypeBuildTarget } = require('./lib/types');
 
-sywac
-  .registerFactory('environment', opts => new TypeEnvironment(opts))
-  .registerFactory('version', opts => new TypeVersion(opts))
-  .registerFactory('service', opts => new TypeService(opts))
-  .registerFactory('build-target', opts => new TypeBuildTarget(opts))
-  .commandDirectory('./cli')
-  .showHelpByDefault()
-  .help('-h, --help')
-  .style(require('./lib/style'));
+try {
+  sywac
+    .registerFactory('environment', opts => new TypeEnvironment(opts))
+    .registerFactory('version', opts => new TypeVersion(opts))
+    .registerFactory('service', opts => new TypeService(opts))
+    .registerFactory('build-target', opts => new TypeBuildTarget(opts))
+    .commandDirectory('./cli')
+    .showHelpByDefault()
+    .help('-h, --help')
+    .style(require('./lib/style'));
+} catch(e) {
+  console.log(e);
+  process.exit(1);
+}
 
 async function main() {
   await sywac.parseAndExit();
