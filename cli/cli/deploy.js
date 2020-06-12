9b9344b99e0a00cdf9fd52e9f9265e35d2adb1fe
jbrunton <john_brunton@hotmail.co.uk>
2020-06-12 12:31:48 +0100

    LIST-197 target list-187-staging branch in CI tasks

--- cli/cli/deploy.js
+++ cli/cli/deploy.js
@@ -17,7 +17,7 @@ module.exports = {
     console.log(`currentVersion: ${currentVersion}, nextVersion: ${nextVersion}`);
 
     const payload = {
-      ref: 'list-197',
+      ref: 'list-187-staging',
       environment: 'update_manifest',
       task: 'update_manifest',
       description: 'New version',
