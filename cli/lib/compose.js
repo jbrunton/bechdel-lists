99860c601f2d8b39a6a383d2ba7d2b66dfa48f03
John Brunton <john_brunton@hotmail.co.uk>
2020-06-20 20:42:00 +0100

    LIST-202 remove references to cadvisor and node_exporter

--- cli/lib/compose.js
+++ cli/lib/compose.js
@@ -39,9 +39,6 @@ class Compose {
   }
 
   async config() {
-    // these images aren't built locally, so we need to pull them to get the digests
-    await exec('docker-compose pull cadvisor nginx_exporter', this.execOpts);
-
     const result = await exec('docker-compose config --resolve-image-digests', this.execOpts);
     const dockerFile = result.stdout;
     const config = removeBuildContexts(dockerFile);
