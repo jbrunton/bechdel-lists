4de521452b4c90155038fd9b2ff1dc2d330e63ae
John Brunton <john_brunton@hotmail.co.uk>
2020-06-20 16:35:02 +0100

    LIST-216 start dev traefik instance in client tests

--- ci/integration_tests/client.sh
+++ ci/integration_tests/client.sh
@@ -6,6 +6,7 @@ npm ci --no-optional
 
 cd ${WORKSPACE}
 docker network create traefik-public
+docker-compose -f docker-compose.traefik-dev.yml up -d
 docker-compose up -d
 docker-compose run api bin/rails db:migrate RAILS_ENV=development
 export COMPOSE_FILE=$($WORKSPACE/get-compose-file.sh cypress)
